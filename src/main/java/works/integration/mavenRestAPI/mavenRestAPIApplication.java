882d09d2d78f116ee8db9a87595021df571e5391
Scaffolder <scaffolder@backstage.io>
2024-04-11 01:50:08 +0000

    initial commit

---
+++ src/main/java/works/integration/mavenRestAPI/mavenRestAPIApplication.java
@@ -0,0 +1,13 @@
+package works.integration.mavenRestAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class mavenRestAPIApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(mavenRestAPIApplication.class, args);
+	}
+
+}
---
+++ src/test/java/works/integration/mavenRestAPI/mavenRestAPIApplicationTests.java
@@ -0,0 +1,13 @@
+package works.integration.mavenRestAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class mavenRestAPIApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
